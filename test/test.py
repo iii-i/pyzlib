94f0d4c1651af244f54664184d3b88cf5699057c
Ilya Leoshkevich <iii@linux.ibm.com>
2019-02-26 13:17:49 +0100

    Add dictionary test

--- test/test.py
+++ test/test.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
 import contextlib
 import ctypes
+import itertools
+import numpy
 import os
 import subprocess
 import tempfile
@@ -7,6 +10,61 @@ import unittest
 
 import parameterized
 import pyzlib
+
+
+def gen_hello(r):
+    while True:
+        yield b'hello\n'
+
+
+def gen_seq(r):
+    i = 0
+    while True:
+        yield ('%d\n' % i).encode()
+        i += 1
+
+
+def gen_nulls(r):
+    while True:
+        yield b'\0' * 4096
+
+
+def gen_zeros_ones(r):
+    while True:
+        yield bytes(r.choice((b'0', b'1'), 4096))
+
+
+def gen_random(r):
+    while True:
+        yield bytes(r.randint(0, 255, 4096, numpy.ubyte))
+
+
+class Gen(object):
+    def __init__(self, chunks):
+        self.chunks = chunks
+        self.buffer = bytearray()
+
+    def __call__(self, n):
+        while len(self.buffer) < n:
+            self.buffer.extend(next(self.chunks))
+        result = self.buffer[:n]
+        del self.buffer[:n]
+        return result
+
+
+def gen_mix(r):
+    gs = [
+        Gen(f(r))
+        for f in (
+            gen_hello,
+            gen_seq,
+            gen_nulls,
+            gen_zeros_ones,
+            gen_random,
+        )
+    ]
+    while True:
+        yield r.choice(gs)(r.randint(1, 65536))
 
 
 class TestCase(unittest.TestCase):
@@ -89,6 +147,12 @@ class TestCase(unittest.TestCase):
         return ctypes.cast(ctypes.addressof(buf) + offset, ctypes.c_char_p)
 
     @staticmethod
+    def _addressof_bytearray(buf):
+        return ctypes.cast(ctypes.addressof(
+            (ctypes.c_char * len(buf)).from_buffer(buf)),
+            ctypes.c_char_p)
+
+    @staticmethod
     def _shl(buf, bits):
         buf_pos = 0
         value = 0
@@ -129,6 +193,98 @@ class TestCase(unittest.TestCase):
             err = pyzlib.inflate(strm, pyzlib.Z_NO_FLUSH)
             self.assertEqual(pyzlib.Z_STREAM_END, err)
             self.assertEqual(b'hello\0', bytes(buf))
+
+    def _set_dictionary(self, strm, gen, size):
+        buf = bytearray(gen(size))
+        err = pyzlib.deflateSetDictionary(
+            strm, self._addressof_bytearray(buf), len(buf))
+        self.assertEqual(pyzlib.Z_OK, err)
+        return buf
+
+    def _gen_buf(self, gen, size, dict):
+        result = bytearray()
+        result += gen(size // 3)
+        result += dict[:size // 3]
+        result += gen(size - len(result))
+        return result
+
+    SET_DICTIONARY_SIZES = [1 << x for x in range(0, 17, 4)]
+
+    @parameterized.parameterized.expand(
+        itertools.product(*([SET_DICTIONARY_SIZES] * 4)))
+    def test_set_dictionary(
+            self, dict1_size, buf2_size, dict3_size, buf4_size):
+        gen = Gen(gen_random(numpy.random.RandomState(2024749321)))
+        with tempfile.NamedTemporaryFile() as zfp:
+            with self._make_deflate_stream(raw=True) as strm:
+                dict1 = self._set_dictionary(strm, gen, dict1_size)
+                buf2 = self._gen_buf(gen, buf2_size, dict1)
+                strm.next_in = self._addressof_bytearray(buf2)
+                strm.avail_in = len(buf2)
+                while True:
+                    zbuf = ctypes.create_string_buffer(4096)
+                    strm.next_out = ctypes.cast(
+                        ctypes.addressof(zbuf),
+                        ctypes.c_char_p)
+                    strm.avail_out = len(zbuf)
+                    err = pyzlib.deflate(strm, pyzlib.Z_BLOCK)
+                    self.assertEqual(pyzlib.Z_OK, err)
+                    zfp.write(zbuf[:len(zbuf) - strm.avail_out])
+                    if strm.avail_out != 0:
+                        break
+                dict3 = self._set_dictionary(strm, gen, dict3_size)
+                buf4 = self._gen_buf(gen, buf4_size, dict1 + dict3)
+                strm.next_in = self._addressof_bytearray(buf4)
+                strm.avail_in = len(buf4)
+                stream_end = False
+                while not stream_end:
+                    zbuf = ctypes.create_string_buffer(4096)
+                    strm.next_out = ctypes.cast(
+                        ctypes.addressof(zbuf),
+                        ctypes.c_char_p)
+                    strm.avail_out = len(zbuf)
+                    err = pyzlib.deflate(strm, pyzlib.Z_FINISH)
+                    if err == pyzlib.Z_STREAM_END:
+                        stream_end = True
+                    else:
+                        self.assertEqual(pyzlib.Z_OK, err)
+                    zfp.write(zbuf[:len(zbuf) - strm.avail_out])
+            zfp.flush()
+            zfp.seek(0)
+            inflated = bytearray()
+            with self._make_inflate_stream(raw=True) as strm:
+                err = pyzlib.inflateSetDictionary(
+                    strm, self._addressof_bytearray(dict1), len(dict1))
+                self.assertEqual(pyzlib.Z_OK, err)
+                stream_end = False
+                while not stream_end:
+                    zbuf = bytearray(zfp.read(256))
+                    if len(zbuf) == 0:
+                        break
+                    strm.next_in = ctypes.addressof(
+                        (ctypes.c_char * len(zbuf)).from_buffer(zbuf))
+                    strm.avail_in = len(zbuf)
+                    while True:
+                        buf = ctypes.create_string_buffer(4096)
+                        strm.next_out = ctypes.cast(
+                            ctypes.addressof(buf),
+                            ctypes.c_char_p)
+                        strm.avail_out = len(buf)
+                        err = pyzlib.inflate(strm, pyzlib.Z_BLOCK)
+                        inflated += buf[:len(buf) - strm.avail_out]
+                        if err == pyzlib.Z_STREAM_END:
+                            stream_end = True
+                            break
+                        if err == pyzlib.Z_BUF_ERROR:
+                            break
+                        self.assertEqual(pyzlib.Z_OK, err)
+                        if strm.data_type & 128 != 0:
+                            if strm.total_out == len(buf2):
+                                err = pyzlib.inflateSetDictionary(
+                                    strm, self._addressof_bytearray(dict3),
+                                    len(dict3))
+                                self.assertEqual(pyzlib.Z_OK, err)
+                self.assertEqual(buf2 + buf4, inflated)
 
 
 if __name__ == '__main__':
